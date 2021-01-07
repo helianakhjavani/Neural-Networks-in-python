77fcef68129bcaca451ebda7ec484465c5433b84
helianakhjavani <68470629+helianakhjavani@users.noreply.github.com>
2021-01-07 23:48:49 +0330

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -38,8 +38,27 @@ for i, species in enumerate(df['class'].unique()):
   
   
   #Plot Histogram
-  df['petal_length'].plot.hist(title='Histogram of Petal Length')
+ df['petal_length'].plot.hist(title='Histogram of Petal Length')
   
   
   #Plot BoxPlot to understand the distrubution of the data based on the first quartile, median and the third quartile:
-  df.plot.box(title = 'Boxplot of Sepal Length and width, and Petal Length and width')
+ df.plot.box(title = 'Boxplot of Sepal Length and width, and Petal Length and width')
+  
+  #Data preprocessing in Pandas
+  #1-Encoding categorical variables: Gender, Day, Country ...
+ df4 = pd.DataFrame({'Day': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']})
+ print(pd.get_dummies(df4))
+  
+ # 2-Imputing missing values
+URL ="https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+df5 =pd.read_csv(URL, names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+# randomly select 10 rows
+random_index = np.random.choice(df5.index, replace = False, size = 10)
+# set the sepal length values of these rows to be None
+df5.loc[random_index, 'sepal_length']= None
+print(df5.isnull().any())
+#another way is to replace the missing value with the mean of the non_missing values
+#df5.sepal_length = df5.sepal_length.fillna(df5.sepal_length.mean())
+#print(df5.isnull().any())
+
+
